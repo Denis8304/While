676cefe9d951c7f1e1d3d348adb7f216e789e123
Denis8304 <60574613+Denis8304@users.noreply.github.com>
2020-02-20 12:10:26 -0300

    Update jsIteraciones (9).js

--- jsIteraciones (9).js
+++ jsIteraciones (9).js
@@ -1,16 +1,28 @@
 function mostrar()
 {
+	
+	var num1 = parseInt(prompt("Ingrese numero:"));
+	var min = parseInt(num1);
+	var max = parseInt(num1);
+	var respuesta = prompt("Desea ingresar otro numero?");
 
-	var contador=0;
-	var respuesta='si';
-
-	while(respuesta!='no')
+	while(respuesta != 'no')
 	{
+		num1 = parseInt(prompt("Ingrese numero:"));
+				
+		if(num1 > max)
+		{
+			max = num1;
+		}
+		else if(num1 < min)
+		{
+			min = num1;
+		}
 		
+	respuesta = prompt("Desea ingresar otro numero?");
 	
 	}
 
-
-
-
-}//FIN DE LA FUNCIÓN
+	document.getElementById("maximo").value = max;
+	document.getElementById("minimo").value = min;
+}
